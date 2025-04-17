007cb080f519fc0f2a643b069f77383855e561b0
yijun09220219 <a113221020@mail.shu.edu.tw>
2025-04-17 12:01:25 +0800

    114-04-17

---
+++ 114_04_17/Tutorial 7-2 -2/Test Average/Test Average/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Test_Average
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Average 方法接受一個 List<int> 參數
+        // 並返回該清單中值的平均值。
+        private double Average(List<int> scores)
+        {
+            int total = 0;
+            foreach (int score in scores)
+            {
+                total += score;
+            }
+            return (double)total / scores.Count;
+        }
+
+        // Highest 方法接受一個 List<int> 參數
+        // 並返回該清單中的最高值。
+        private int Highest(List<int> scores)
+        {
+            int highest = scores[0];
+            for (int i = 1; i < scores.Count; i++)
+            {
+                if (scores[i] > highest)
+                {
+                    highest = scores[i];
+                }
+            }
+            return highest;
+        }
+
+        // Lowest 方法接受一個 List<int> 參數
+        // 並返回該清單中的最低值。
+        private int Lowest(List<int> scores)
+        {
+            int lowest = scores[0];
+            foreach (int score in scores)
+            {
+                if (score < lowest)
+                {
+                    lowest = score;
+                }
+            }
+            return lowest;
+        }
+
+        private void getScoresButton_Click(object sender, EventArgs e)
+        {
+            List<int> testScores = new List<int>();
+            int highestScore = 0;
+            int lowestScore = 0;
+            double averageScore = 0.0;
+            StreamReader inputFile;
+            try
+            {
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    // 打開文件。
+                    inputFile = File.OpenText(openFile.FileName);
+                    // 從文件中讀取測試分數。
+                    while (!inputFile.EndOfStream)
+                    {
+                        testScores.Add(Convert.ToInt32(inputFile.ReadLine()));
+                    }
+                    // 關閉文件。
+                    inputFile.Close();
+                    // 計算平均分數、最高分數和最低分數。
+                    averageScore = Average(testScores);
+                    highestScore = Highest(testScores);
+                    lowestScore = Lowest(testScores);
+                    // 顯示結果。
+                    averageScoreLabel.Text = averageScore.ToString("n1");
+                    highScoreLabel.Text = highestScore.ToString();
+                    lowScoreLabel.Text = lowestScore.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "錯誤");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單。
+            this.Close();
+        }
+    }
+}
+
---
+++ 114_04_17/Tutorial 7-2/Test Average/Test Average/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Test_Average
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Average 方法接受一個 int 陣列參數
+        // 並返回該陣列中值的平均值。
+        private double Average(int[] scores)
+        {
+            int total = 0;
+            foreach (int score in scores)
+            {
+                total += score;
+            }
+            return (double)total / scores.Length;
+        }
+
+        // Highest 方法接受一個 int 陣列參數
+        // 並返回該陣列中的最高值。
+        private int Highest(int[] scores)
+        {
+            int highest = scores[0];
+            for (int i=1;i<scores.Length;i++)
+            {
+                if (scores[i] > highest)
+                {
+                    highest = scores[i];
+                }
+            }
+            return highest;
+        }
+
+        // Lowest 方法接受一個 int 陣列參數
+        // 並返回該陣列中的最低值。
+        private int Lowest(int[] scores)
+        {
+            int lowest = scores[0];
+            foreach (int score in scores)
+            {
+                if (score < lowest)
+                {
+                    lowest = score;
+                }
+            }
+            return lowest;
+        }
+
+        private void getScoresButton_Click(object sender, EventArgs e)
+        {
+            const int SIZE = 48;
+            int[] testScores = new int[SIZE];
+            int index = 0;
+            int highestScore = 0;
+            int lowestScore = 0;
+            double averageScore = 0.0;
+            StreamReader inputFile;
+            try
+            {
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    // 打開文件。
+                    inputFile = File.OpenText(openFile.FileName);
+                    // 從文件中讀取測試分數。
+                    while (!inputFile.EndOfStream)
+                    {
+                        testScores[index] = Convert.ToInt32(inputFile.ReadLine());
+                        index++;
+                    }
+                    // 關閉文件。
+                    inputFile.Close();
+                    // 計算平均分數、最高分數和最低分數。
+                    averageScore = Average(testScores);
+                    highestScore = Highest(testScores);
+                    lowestScore = Lowest(testScores);
+                    // 顯示結果。
+                    averageScoreLabel.Text = averageScore.ToString("n1");
+                    highScoreLabel.Text = highestScore.ToString();
+                    lowScoreLabel.Text = lowestScore.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "錯誤");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單。
+            this.Close();
+        }
+    }
+}
+
